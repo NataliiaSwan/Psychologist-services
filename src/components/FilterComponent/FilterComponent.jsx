396abdbd463fd5a55a8efe832683a8fd07781d66
NataliiaSwan <shapnat69@gmail.com>
2025-03-04 22:22:18 +0200

    Add filter

--- src/components/FilterComponent/FilterComponent.jsx
+++ src/components/FilterComponent/FilterComponent.jsx
@@ -1,22 +1,20 @@
 import css from "../../components/FilterComponent/FilterComponent.module.css";
 
-const FilterComponent = (onSort) => {
+const FilterComponent = () => {
   return (
-    <div>
-      <p>Filter</p>
-      <button className={css.buttonAZ}>A to Z</button>
+    <div className={css.filterContainer}>
+      <h1>Filter</h1>
+      <button className={css.button}>A to Z</button>
       <div className={css.boxfilter}>
-        <button onClick={() => ("name", "asc")}>A-Z</button>
-        <button onClick={() => ("name", "desc")}>Z-A</button>
-        <button onClick={() => onSort("price_per_hour", "asc")}>
-          Less than 10$
-        </button>
-        <button onClick={() => onSort("price_per_hour", "desc")}>
-          Greater than 10$
-        </button>
-        <button onClick={() => onSort("popular", "asc")}>Popular</button>
-        <button onClick={() => onSort("popular", "asc")}>Not popular</button>
-        <button onClick={() => onSort("all", "asc")}>Show all</button>
+        <ul className={css.filterList}>
+          <li>A to Z</li>
+          <li>Z to A</li>
+          <li>Less than 10$</li>
+          <li>Greater than 10$</li>
+          <li>Popular</li>
+          <li>Not popular</li>
+          <li>Show all</li>
+        </ul>
       </div>
     </div>
   );
