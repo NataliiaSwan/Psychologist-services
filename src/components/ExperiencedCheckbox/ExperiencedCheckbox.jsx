240c63d6764e7c4ad2a6df78a73a2ea33066cb2d
NataliiaSwan <shapnat69@gmail.com>
2025-05-10 12:12:46 +0300

    update components

--- src/components/ExperiencedCheckbox/ExperiencedCheckbox.jsx
+++ src/components/ExperiencedCheckbox/ExperiencedCheckbox.jsx
@@ -1,40 +1,10 @@
 import css from "./ExperiencedCheckbox.module.css";
 
-// const ExperiencedCheckbox = ({ checked, onCheckboxChange }) => {
-//   const handleChange = (e) => {
-//     onCheckboxChange(e.target.checked);
-//   };
-
-//   return (
-//     <div className={css.checkboxcontainer}>
-//       <label className={css.checkboxlabel}>
-//         <input
-//           type="checkbox"
-//           checked={checked}
-//           onChange={handleChange}
-//           className={css.checkbox}
-//         />
-//         <div className={css.customCheckbox}></div>
-//         <div className={css.textContainer}>
-//           <span className={css.text}>Experienced psychologists</span>
-//           <span className={css.count}>15,000</span>
-//         </div>
-//       </label>
-//     </div>
-//   );
-// };
-
-// export default ExperiencedCheckbox;
-
 const ExperiencedCheckbox = ({
   checked,
   onCheckboxChange,
   experiencedCount,
 }) => {
-  // const handleChange = (e) => {
-  //   onCheckboxChange(e.target.checked);
-  // };
-
   const handleChange = (e) => {
     const checked = e.target.checked;
     onCheckboxChange(checked);
